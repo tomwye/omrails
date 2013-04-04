1f1189584147fe9ee457d6c01e6c94cbf519f06b
Tom Wye <tom@originalsinevents.co.uk>
2013-04-04 19:28:55 +0100

    Reroute root to pins#index

--- app/controllers/pins_controller.rb
+++ app/controllers/pins_controller.rb
@@ -1,5 +1,5 @@
 class PinsController < ApplicationController
-  before_filter :authenticate_user!, exception: [:index]
+  before_filter :authenticate_user!, except: [:index]
   # GET /pins
   # GET /pins.json
   def index
